dc047596777dcde1a27328db51b5da4bf3cb7b6d
Fernando Poli <fernandoandrespoli@gmail.com>
2019-11-26 21:03:40 -0300

    first commit

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? 'https://go.bairesdev.com/icons/'
-    : '/'
+  publicPath: process.env.NODE_ENV === "production" ? "/" : "/"
 };
